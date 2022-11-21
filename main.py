caa5d5e9180e2dc14b6c605907f9d4a9b1dd10a9
Jerry Kurtin <gerald.kurtin7@gmail.com>
2022-11-21 17:35:19 -0600

    first working prototype!

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import objects.board as board
 
 if __name__ == "__main__":
-    my_board = board.Board()
+    my_board = board.Board(4, 64)
     bst = board.state.CONTINUE
 
 
     print("Welcome to 2048! Control the board with wasd")
+    print("claim opponents' squares on your move")
+    print("after you move, your opponent gets a new 2 square, so be careful!")
+    print(f"the first person to steal all squares or reach a square of {my_board.win_state} wins!")
 
     while bst == board.state.CONTINUE:
-        print("\nCurrent board:")
+        print("\n\n")
         print(my_board)
-        print("\nCurrent owner:")
-        print("\n\n".join(["\t".join(list(map(str, row))) for row in my_board.owner]))
-        dir = input()
+        print(f"P1 score: {my_board.p1score}, P2 score: {my_board.p2score}")
+        # print("\nCurrent owner:")
+        # print("\n\n".join(["\t".join(list(map(str, row))) for row in my_board.owner]))
+        dir = input(f"\nPlayer {my_board.player + 1}, enter your move -> ")
         try:
             bst = my_board.move(dir)
         except ValueError:
@@ -20,7 +24,14 @@ if __name__ == "__main__":
 
     print("Final state:")
     print(my_board)
-    if bst == board.state.WIN:
-        print("You won!")
+    if bst == board.state.WIN1:
+        print("Player 1 won!")
+    elif bst == board.state.WIN2:
+        print("Player 2 won!")
     else:
-        print("you lost :(")
+        if my_board.p1score > my_board.p2score:
+            print("Player 1 wins by score!")
+        elif my_board.p1score < my_board.p2score:
+            print("Player 2 wins by score!")
+        else:
+            print("It's a tie!")
--- objects/board.py
+++ objects/board.py
@@ -2,14 +2,15 @@ from random import randint, sample
 from enum import Enum
 
 class state(Enum):
-    WIN = 1
+    WIN2 = 2
+    WIN1 = 1
     CONTINUE = 0
     LOSS = -1
 
 class Board:
-    def __init__(self, board_size=4):
+    def __init__(self, board_size=4, win_state=2048):
         self.board_size = board_size
-        self.win_state = 16
+        self.win_state = win_state
         self.squares_added = 1
         self.player = 0
 
@@ -19,13 +20,16 @@ class Board:
         # initialize the first numbers
         st1 = [randint(0, self.board_size - 1), randint(0, self.board_size - 1)]
         st2 = [randint(0, self.board_size - 1), randint(0, self.board_size - 1)]
-        while (st1 == st2):
+        while (st1[0] == st2[0] or st1[1] == st2[1]):
             st2 = [randint(0, self.board_size - 1), randint(0, self.board_size - 1)]
         self.board[st1[0]][st1[1]] = 2
         self.owner[st1[0]][st1[1]] = 0
 
         self.board[st2[0]][st2[1]] = 2
         self.owner[st2[0]][st2[1]] = 1
+
+        self.p1score = 2
+        self.p2score = 2
 
     def move(self, dir):
         # call the proper move function
@@ -43,25 +47,46 @@ class Board:
         
         # check for win condition and add squares
         cands = []
+        self.p1score = 0
+        self.p2score = 0
         for row in range(len(self.board)):
             for col in range(len(self.board[0])):
                 # win!
                 if self.board[row][col] == self.win_state:
-                    return state.WIN
+                    if self.owner[row][col] == 0:
+                        return state.WIN1
+                    else:
+                        return state.WIN2
+                
                 # empty square
                 elif self.board[row][col] == 0:
                     cands.append((row, col))
+                # track ids to see if anybody has been eliminated
+                else:
+                    if self.owner[row][col] == 0:
+                        self.p1score += self.board[row][col]
+                    else:
+                        self.p2score += self.board[row][col]
         
+        # elimination win
+        if self.p1score == 0:
+            return state.WIN2
+        elif self.p2score == 0:
+            return state.WIN1
 
         # add new values to board
         added = sample(cands, self.squares_added)
         for val in added:
             self.board[val[0]][val[1]] = 2
             self.owner[val[0]][val[1]] = self.player
+            if self.player == 0:
+                self.p1score += 2
+            else:
+                self.p2score += 2
         
         # switch to the next player
         self.player = (self.player + 1) % 2
-
+        
         # check for loss
         if len(cands) <= self.squares_added:
             return state.LOSS
@@ -72,7 +97,7 @@ class Board:
         """The function to make a left movement"""
         for row in range(len(self.board)):
             newrow = [0] * len(self.board)
-            newowner = self.owner[row].copy()
+            newowner = [-1] * len(self.board)
             newidx = 0
             
 
@@ -98,15 +123,13 @@ class Board:
             self.board[row] = list(map(abs, newrow))
 
             # update the owner
-            while (newidx < len(newowner)):
-                newowner[newidx] = -1
-                newidx += 1
             self.owner[row] = newowner
     
     def right(self):
         """The function to make a right movement"""
         for row in range(len(self.board)):
             newrow = [0] * len(self.board)
+            newowner = [-1] * len(self.board)
             newidx = len(self.board[0]) - 1
 
             for col in range(len(self.board[0]) - 1, -1, -1):
@@ -116,18 +139,27 @@ class Board:
                     if newidx < len(self.board[0]) - 1 and self.board[row][col] == newrow[newidx + 1]:
                         newrow[newidx + 1] = newrow[newidx + 1] * -2    # negative prevents double merge
 
+                        # determine ownership of new square
+                        if newowner[newidx + 1] != self.owner[row][col]:
+                            newowner[newidx + 1] = self.player
+
                     # add normally
                     else:
                         newrow[newidx] = self.board[row][col]
+                        newowner[newidx] = self.owner[row][col]
                         newidx -= 1
 
             # update the row
             self.board[row] = list(map(abs, newrow))
 
+            # update the owner
+            self.owner[row] = newowner
+
     def up(self):
         """The function to make an up movement"""
         for col in range(len(self.board[0])):
             newcol = [0] * len(self.board)
+            newowner = [-1] * len(self.board)
             newidx = 0
 
             for row in range(len(self.board)):
@@ -137,19 +169,26 @@ class Board:
                     if newidx > 0 and self.board[row][col] == newcol[newidx - 1]:
                         newcol[newidx - 1] = newcol[newidx - 1] * -2    # negative prevents double merge
 
+                        # determine ownership of new square
+                        if newowner[newidx - 1] != self.owner[row][col]:
+                            newowner[newidx - 1] = self.player
+
                     # add normally
                     else:
                         newcol[newidx] = self.board[row][col]
+                        newowner[newidx] = self.owner[row][col]
                         newidx += 1
 
-            # update the row
+            # update the row and owner
             for row in range(len(self.board)): 
                 self.board[row][col] = abs(newcol[row])
+                self.owner[row][col] = newowner[row]
     
     def down(self):
         """The function to make a down movement"""
         for col in range(len(self.board[0])):
             newcol = [0] * len(self.board)
+            newowner = [-1] * len(self.board)
             newidx = len(self.board) - 1
 
             for row in range(len(self.board) - 1, -1, -1):
@@ -159,19 +198,27 @@ class Board:
                     if newidx < len(self.board) - 1 and self.board[row][col] == newcol[newidx + 1]:
                         newcol[newidx + 1] = newcol[newidx + 1] * -2    # negative prevents double merge
 
+                        # determine ownership of new square
+                        if newowner[newidx + 1] != self.owner[row][col]:
+                            newowner[newidx + 1] = self.player
+
                     # add normally
                     else:
                         newcol[newidx] = self.board[row][col]
+                        newowner[newidx] = self.owner[row][col]
                         newidx -= 1
 
             # update the row
             for row in range(len(self.board)): 
                 self.board[row][col] = abs(newcol[row])
+                self.owner[row][col] = newowner[row]
 
     def __repr__(self):
-        printarr = self.board.copy()
+        printarr = [[col for col in row] for row in self.board]
         for row in range(len(printarr)):
             for col in range(len(printarr[0])):
+                if printarr[row][col] == 0:
+                    printarr[row][col] = "."
                 if self.owner[row][col] == 1:
                     printarr[row][col] *= -1
 
@@ -180,26 +227,4 @@ class Board:
         
 
 if __name__ == "__main__":
-    my_board = Board()
-    bst = state.CONTINUE
-
-
-    print("Welcome to 2048! Control the board with wasd")
-
-    while bst == state.CONTINUE:
-        print("\nCurrent board:")
-        print(my_board)
-        print("\nCurrent owner:")
-        print("\n\n".join(["\t".join(list(map(str, row))) for row in my_board.owner]))
-        dir = input()
-        try:
-            bst = my_board.move(dir)
-        except ValueError:
-            print("invalid move, try again")
-
-    print("Final state:")
-    print(my_board)
-    if bst == state.WIN:
-        print("You won!")
-    else:
-        print("you lost :(")
+    pass
