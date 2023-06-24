b89a20b1563cbd82824a30d17fcc57167c015066
Jerry Kurtin <gerald.kurtin7@gmail.com>
2023-06-23 23:01:53 -0500

    basic lobby functionality

--- client/src/OnlineLobby.js
+++ client/src/OnlineLobby.js
@@ -1,25 +1,64 @@
 import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/esm/Button";
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://192.168.1.231:4000";
 
 function OnlineLobby() {
     const [response, setResponse] = useState("");
+    const [playersOnline, setPlayersOnline] = useState(0);
+    const [onlineStatus, setOnlineStatus] = useState("lobby");  // "lobby", "waiting", "privateWaiting", "inGame"
+    const [username, setUsername] = useState("");
 
+    // start and end socket connnection
     useEffect(() => {
         const socket = socketIOClient(ENDPOINT);
-        socket.on("FromAPI", data => {
+
+        socket.on("TestTime", data => {
             console.log("response received!");
             setResponse(data);
         });
+
+        socket.on("OnlineCount", (pOnline) => {
+            console.log("new player");
+            setPlayersOnline(pOnline);
+        })
+
+        socket.on("username", (newUsername) => {
+            console.log("new username given: ", newUsername);
+            setUsername(newUsername);
+        })
+
+        // clean up
+        return () => socket.disconnect();
     }, []);
+
+    function loadView(status) {
+        if (status === "lobby") {
+            return (<>
+                <h1>Lobby</h1>
+                <Button onClick={() => setOnlineStatus("waiting")}>Join Random Match</Button>
+                <Button>Start Local Match</Button>
+                <Button>Join Local Match</Button>
+            </>);
+        }
+        
+        else if (status === "waiting") {
+            return (<>
+                <h1>Waiting for Match...</h1>
+                <Button onClick={() => setOnlineStatus("lobby")}>Return</Button>
+    
+            </>);
+        }
+    }
 
     return (
     <div>
-        <h1>Waiting for Match...</h1>
-        <p>people in lobby: 1</p>
+        <p>{"Username: " + username}</p>
+        <p>{"Players online: " + playersOnline}</p>
         <p>It's <time dateTime={response}>{response}</time></p>
+        { loadView(onlineStatus) }
     </div>
     )
 }
 
-export default OnlineLobby
+export default OnlineLobby;
--- server/app.js
+++ server/app.js
@@ -1,15 +1,3 @@
-// const express = require("express");
-// const socket = require("socket.io");
-
-// const PORT = process.env.PORT || 4000;
-
-// const app = express();
-// const server = app.listen(PORT, function () {
-//   console.log(`Listening on port ${PORT}`);
-//   console.log(`http://localhost:${PORT}`);
-// });
-
-
 const express = require('express');
 const cors = require('cors');
 const http = require("http");
@@ -29,23 +17,90 @@ const io = socketIo(server, {
       }
 });
 
-let interval;
+// ------------------------ Settings ------------------------
+const GUEST_ID_LENGTH = 5;
+
+// ----------------------------------------------------------
+
+// ------------------------ Server Information ------------------------
+const activeUsers = new Set();
+// map a user to their socket
+const clientMap = new Map();
+var playersOnline = 0;
+
+// --------------------------------------------------------------------
+
+let intervals = {};
 io.on("connection", (socket) => {
-  console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 100);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    clearInterval(interval);
-  });
+    console.log("New client connected");
+    const newUsername = addUser(socket);
+    clientMap.set(newUsername, socket);
+    console.log(clientMap.get(newUsername).id);
+
+    intervals[socket.id] = setInterval(() => emitTestTime(socket), 1000);
+
+    // disconnect a user
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+        clearInterval(intervals[socket.id]);
+        delete intervals[socket.id];
+
+        removeUser(socket, newUsername);
+        clientMap.delete(newUsername);
+
+    });
 });
 
-const getApiAndEmit = socket => {
-    const response = new Date();
-    // Emitting a new message. Will be consumed by the client
-    socket.emit("FromAPI", response);
-};
+// print server info for debugging
+// setInterval(() => {
+//     console.log("active users", activeUsers);
+//     console.log("client mappings length:", clientMap.size);
+// }, 3000)
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+// ------------------------ Helpers ------------------------
+
+/**
+ * emits the current time
+ */
+const emitTestTime = socket => {
+    const response = new Date();
+    // Emitting a new message. Will be consumed by the client
+    socket.emit("TestTime", response);
+};
+
+/**
+ * Add a new user
+ */
+function addUser(socket) {
+    playersOnline++;
+    var newUsername;
+    do {
+        newUsername = generateGuestId(GUEST_ID_LENGTH);
+    } while (activeUsers.has(newUsername));
+
+    activeUsers.add(newUsername);
+    io.emit("OnlineCount", playersOnline);
+    socket.emit("username", newUsername);
+    return newUsername;
+}
+
+/**
+ * Remove user
+ * TODO: remove username and mapping
+ */
+function removeUser(socket, username) {
+    playersOnline--;
+    activeUsers.delete(username)
+    io.emit("OnlineCount", playersOnline);
+}
+
+function generateGuestId(idLength) {
+    let result = 'guest-';
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < idLength; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
