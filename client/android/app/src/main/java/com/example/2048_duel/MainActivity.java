a378bc6b39d3a24f004fc868b812a63f78aca569
Jerry Kurtin <gerald.kurtin7@gmail.com>
2023-09-15 19:51:38 +0800

    added andriod support and got safe space to work

---
+++ client/android/app/src/main/java/com/example/2048_duel/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.2048_duel;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -4,9 +4,11 @@ const config: CapacitorConfig = {
   appId: 'com.example.2048_duel',
   appName: '2048_duel',
   webDir: 'build',
-  server: {
-    androidScheme: 'https'
-  }
+  bundledWebRuntime: false,
+  // server: {
+  //   url: 'http://192.168.0.28:3000',
+  //   cleartext: true
+  // }
 };
 
 export default config;
