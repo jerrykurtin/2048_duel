26970153abab61d766900a52d60f9ec87d49c6fd
Jerry Kurtin <gerald.kurtin7@gmail.com>
2023-10-06 13:17:43 +0800

    fixed a few logo issues, made entry screen more colorful and animated

--- client/src/elements/MenuOption.js
+++ client/src/elements/MenuOption.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, {useState} from "react";
 import Card from "react-bootstrap/Card";
 
-function MenuOption({title, contents, disabled=false, ...props}) {
+import "./MenuOption.css";
+
+function MenuOption({title, contents, disabled=false, onClick, ...props}) {
+  const [clicked, setClicked] = useState(false);
+
+  function setClickedWithTimeout() {
+    setClicked(true);
+    setTimeout( () => {
+      setClicked(false);
+    }, ((disabled) ? (200) : 500));
+  }
+
   return (
-    <Card className={"mode-select" + ((disabled === true) ? " coming-soon" : " cool-fill")} border="secondary" {...props}>
-        <Card.Header className="text-center">{title}</Card.Header>
-        <Card.Body>
-            <Card.Text>{contents}</Card.Text>
-        </Card.Body>
-    </Card>
+    <div onClick={() => {
+      setClickedWithTimeout(true);
+      onClick();
+    }}className={"custom-card mode-select text-center" + ((disabled === true) ? " coming-soon" : " cool-fill") + ((clicked == true) ? " selected" : "")} border="secondary" {...props}>
+      <div className="custom-card-header">
+        <h3>{title}</h3>
+      </div>
+      <div className="fill-remaining-space">
+        <div className="vertical-aligner">
+          <div className="custom-card-body">{contents}</div>
+        </div>
+      </div>
+    </div>
+    // <Card className={"mode-select" + ((disabled === true) ? " coming-soon" : " cool-fill")} border="secondary" {...props}>
+    //     <Card.Header className="text-center">{title}</Card.Header>
+    //     <Card.Body>
+    //         <Card.Text>{contents}</Card.Text>
+    //     </Card.Body>
+    // </Card>
   )
 }
 
