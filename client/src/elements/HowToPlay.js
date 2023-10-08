ec8082b6eddfdf60a9f8b87dd2ea890016f5e0f5
Jerry Kurtin <gerald.kurtin7@gmail.com>
2023-10-08 20:44:34 +0800

    small update to instructions

--- client/src/elements/HowToPlay.js
+++ client/src/elements/HowToPlay.js
@@ -5,14 +5,14 @@ function HowToPlay({winningPiece, ...props}) {
     <div {...props}>
       <h5>Controls</h5>
         <ul>
-          <li>Swipe or use arrow keys to move all squares in a direction.</li>
+          <li>Swipe or use arrow keys to move all squares in a direction</li>
           <li>Collide two squares of the same number to combine them and double the number</li>
         </ul>
         <h5>Duel</h5>
         <ul>
-          <li>Your pieces are color-coded</li>
+          <li>Your squares are color-coded</li>
           <li>On your turn, collide one of your squares with your oponnent's to steal it</li>
-          <li>The first to create the {winningPiece} square wins!</li>
+          <li>The first to create the {winningPiece} square in their color wins!</li>
         </ul>
     </div>
   )
