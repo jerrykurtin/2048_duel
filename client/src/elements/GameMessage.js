10d66730d03f87389fec3c417ab140ecc3b4203f
Jerry Kurtin <gerald.kurtin7@gmail.com>
2024-04-03 18:19:25 -0500

    replaced old button

--- client/src/elements/GameMessage.js
+++ client/src/elements/GameMessage.js
@@ -3,13 +3,22 @@ import Button from 'react-bootstrap/Button';
 
 import "./GameMessage.css"
 
+import PopBox from './PopBox';
+
 function GameMessage({color, text, isButton=false, onClick=null}) {
     return (
         <div>
             <div className="endgame-bkgd"/>
             <div className="endgame-msg">
                 {(isButton)
-                ? <Button variant="outline-main-color" className="game-msg game-msg-button " onClick={() => onClick()}>{text}</Button>  
+                ? <div className="game-msg centered">
+                    <PopBox color="accent" onClick={() => onClick()}>
+                        <div className="centered game-msg-start">
+                            <div>{text}</div>
+                        </div>
+                    </PopBox>
+                </div>
+                // <Button variant="outline-main-color" className="game-msg game-msg-button " onClick={() => onClick()}>{text}</Button>  
                 : <div className={"game-msg text " + color}>{text}</div>}
             </div>
         </div>
