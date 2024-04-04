1ffdf78dafa5a0cccd549f8b6538d5896bd69c65
Jerry Kurtin <gerald.kurtin7@gmail.com>
2024-04-04 14:52:30 -0500

    added privacy policy

---
+++ client/src/PrivacyPolicy.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PrivacyPolicy() {
+    return (
+        <div>
+            <h1>Privacy Policy</h1>
+            <p>{"I don't collect or share any information, and I don't run any ads. This is a simple game I made to get better at coding."}</p>
+            <p>{"There is currently no online interaction, and all aspects of the app are safe for children. Enjoy :)"}</p>
+        </div>
+    )
+}
+
+export default PrivacyPolicy
